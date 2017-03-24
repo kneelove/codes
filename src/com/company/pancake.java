339d28093618087fd73f0ab6135b177cc732a1db
nilavvaghasiya <nilav.vaghasiya@gmail.com>
2017-03-24 13:29:32 +0530

    pancake code
    first commit

---
+++ src/com/company/pancake.java
@@ -0,0 +1,52 @@
+package com.company;
+/**
+ * Created by nilav on 3/23/2017.
+ */
+import java.io.*;
+import java.util.Scanner;
+
+public class pancake {
+    public static void main(String ar[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Stack");
+        String s = sc.next();
+
+
+        calculate(s);
+    }
+    public static void calculate(String a)
+    {
+        String sub = flipper(a);
+        System.out.println(a);
+        valchange(sub);
+    }
+
+    public static String flipper(String a) {
+
+        int position = findval(a);
+
+        String c = a.substring(0, (position-1));
+        System.out.println("substring is "+ c);
+        return c;
+    }
+
+    public static int findval(String arr) {
+        int position = 0;
+        for (int i = 0; i < arr.length(); i++) {
+            if (arr.charAt(i) == '+') {
+                position = i+1;
+            }
+        }
+        return position;
+    }
+    public static void valchange(String s)
+    {char[] buf = new char[s.length()];
+        for(int i=0;i<s.length();i++)
+        { buf[i]=s.charAt(i)=='-'?'+':'-';
+
+        }
+        buf.toString();
+        System.out.println(buf);
+
+
+    }}
